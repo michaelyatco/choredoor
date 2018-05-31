535d837a170059a2726754b6aec3f9fe257c19ff
michaelyatco <mjyatco@gmail.com>
2018-05-31 10:51:55 -0400

    First Mardkdown draft of opening steps

---
+++ ChoreDoorIntro.md
@@ -0,0 +1,69 @@
+##Milestone Project: Avoid the ChoreBot!
+
+###Bringing It All Together
+
+Let's build a Milestone Project utilizing all the HTML, CSS, and JavaScript concepts covered in the previous modules of your **Web Development Path**! 
+
+###Project Overview
+Your mission is to construct a single-page website that plays a fully-functional game. You will see how HTML, CSS, and JavaScript interact harmoniously to produce a dynamic website and hopefully, you'll have fun along the way!
+
+###Getting Started - Get a door, open a door!
+
+1. [ ] Look at the **index.html** starting code and then navigate to the **images** folder. In between the`<div class=doorRow>``</div>`, create an `<img>` tag for the `closed_door.svg` image and assign that tag an `id` attribute with the value of `door1`.
+
+    >Hint - Make sure to wrap your link in quotation marks:    `<img src="image-location.jpg" />`. You can add the `id` attribute inside the `<img>` tag - just make sure your `id` is also wrapped in quotation marks.
+
+2. [ ] Navigate to the **style.css** starting code and create an `id` selector for the `door1`. Add one attribute so that a cursor becomes a pointer whenever you scroll over the image.
+
+    >Hint - An ID attribute is constructed with:
+`#id { property: value;}`
+`cursor: pointer;`
+
+3. [ ] Now we have a pointer that hover overs our closed door image, but it's the JavaScript that brings our image to life! Between the `<script>``</script>` create a global variable called `doorImage1` that accepts `door1` as its value.
+
+    >Hint - Use the DOM method: `document.getElementById('id')`
+
+4. [ ] Give `doorImage1` an `onclick` event using arrow function syntax. The function will be empty for now.
+
+    >Hint - `object.onclick = () => {myScript};`
+    
+5. [ ] Finally, let's have our closed door image change when we click it so that we see an open door with the ChoreBot ready to greet us!  Find the ChoreBot image in the **images** folder and assign that path to a global variable called `botDoor`. Then within our arrow function, change the `src` of `doorImage1` to the value of `botDoor`.
+
+    >Hint - Global variables are declared outside of     	functions.
+    
+    >Hint - An object's `src` value can be assigned a new 	value with:
+   `obj.src = 'new image path'`
+
+6. [ ] Click on the door and watch as the closed door image changes to the ChoreBot ready to greet us with housework! 
+
+```
+Solution Code - HTML
+<!DOCTYPE html>
+  <head></head>
+  <body>
+    <div class="door-row">
+      <img src="images/closed_door.svg" id="door1">
+    </div>
+  </body>
+  <script></script>
+</html>
+```
+```
+Solution Code - CSS
+#door1 {
+  cursor: pointer;
+}
+```
+
+```
+Solution Code - JavaScript
+
+<script>
+let doorImage1 = document.getElementById('door1');
+let botDoor = "images/Robot_open_door.svg";
+
+doorImage1.onclick = () => {
+  doorImage1.src = botDoor;
+}
+</script>
+
---
+++ script.js
@@ -0,0 +1,141 @@
+// //Default global values:
+// let highScore = 0;
+// let score = 0;
+// let doors;
+// let door1Clicked;
+// let door2Clicked;
+// let door3Clicked;
+// let currentlyPlaying = false;
+// let door1Doom;
+// let door2Doom;
+// let door3Doom;
+// let doorImage1 = document.getElementById('door1');
+// let doorImage2 = document.getElementById('door2');
+// let doorImage3 = document.getElementById('door3');
+// let startButton = document.getElementById("start");
+// let scoreDisplay = document.getElementById('score-number');
+// let highScoreDisplay = document.getElementById('high-score-number');
+// let openDoor1;
+// let openDoor2;
+// let openDoor3;
+// let closedDoor = "images/closed_door.svg";
+// let botDoor = "images/Robot_open_door.svg";
+// let beachDoor = "images/beach_open_door.svg";
+// let spaceDoor = "images/space_open_door.svg";
+// scoreDisplay.innerHTML = score;
+// highScoreDisplay.innerHTML = highScore;
+
+// //Prevents mid-round reset
+// startButton.onclick = () => {
+//   if(!currentlyPlaying) {
+//     startRound();
+//   }
+// }
+
+// //Reset values for extra rounds and call new chore door function:
+// const startRound = () => {
+//   currentlyPlaying = true;
+//   door1Clicked = false;
+//   door2Clicked = false;
+//   door3Clicked = false;
+//   door1Doom = false;
+//   door2Doom = false;
+//   door3Doom = false;
+//   doors = 3;
+//   doorImage1.src = closedDoor;
+//   doorImage2.src = closedDoor;
+//   doorImage3.src = closedDoor;
+//   randomChoreDoorGenerator();
+//   startButton.innerHTML = 'Good luck!';
+// }
+
+// //One door is assigned the game-ending boolean value of doorDoom.
+// //The door variables are assigned appropriate src values.
+// const randomChoreDoorGenerator = () => {
+//   choreDoor = Math.floor(Math.random() * doors);
+//   if (choreDoor === 0) {
+//     door1Doom = true;
+//     openDoor1 = botDoor;
+//     openDoor2 = beachDoor;
+//     openDoor3 = spaceDoor;
+//   } else if (choreDoor === 1) {
+//     door2Doom = true;
+//     openDoor2 = botDoor;
+//     openDoor1 = beachDoor;
+//     openDoor3 = spaceDoor;
+//   } else {
+//     door3Doom = true;
+//     openDoor3 = botDoor;
+//     openDoor1 = beachDoor;
+//     openDoor2 = spaceDoor;
+//   }
+// }
+
+// //Doors variable decreases by 1, clicked door cannot be clicked again, check to see if winning or losing condition met.
+// //The doorImage variable receives the correct src value obtained from randomChoreDoorGenerator().
+// //If neither condition is met, play continues.
+// doorImage1.onclick = () => {
+//   if (!door1Clicked) {
+//     door1Clicked = true;
+//     doors--;
+//     doorImage1.src = openDoor1;
+//     if (doors === 0) {
+//       winnerGameOver();
+//     } else if (door1Doom) {
+//       gameOver();
+//     }
+//   }
+// }
+// doorImage2.onclick = () => {
+//   if (!door2Clicked) {
+//     door2Clicked = true;
+//     doors--;
+//     doorImage2.src = openDoor2;
+//     if (doors === 0) {
+//       winnerGameOver();
+//     } else if (door2Doom) {
+//       gameOver();
+//     }
+//   }
+// }
+// doorImage3.onclick = () => {
+//   if (!door3Clicked) {
+//     door3Clicked = true;
+//     doors--;
+//     doorImage3.src = openDoor3;
+//     if (doors === 0) {
+//       winnerGameOver();
+//     } else if (door3Doom) {
+//       gameOver();
+//     }
+//   }
+// }
+
+// //gameOver function for losers
+// const gameOver = () => {
+//   score = 0;
+//   door1Clicked = true; //Prevents opening the other doors and overriding the loss
+//   door2Clicked = true;
+//   door3Clicked = true;
+//   scoreDisplay.innerHTML = score;
+//   determineHighScore();
+// }
+
+// //gameOver function for winners
+// const winnerGameOver = () => {
+//   score++;
+//   scoreDisplay.innerHTML = score;
+//   determineHighScore();
+// }
+
+// //Sets new high score if condition met
+// const determineHighScore = () => {
+//   currentlyPlaying = false;
+//   startButton.innerHTML = 'Play again? Reset!';
+//   if (score > highScore) {
+//     highScore = score;
+//     highScoreDisplay.innerHTML = highScore;
+//   }
+// }
+
+// startRound();
